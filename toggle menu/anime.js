151ec534f96896870b9f60424a0d4461bc0aefa7
Avinash2303 <57543380+Avinash2303@users.noreply.github.com>
2020-01-07 20:57:20 +0530

    added some new files

---
+++ toggle menu/anime.js
@@ -0,0 +1,5 @@
+function clickMenu() {
+    document.getElementById("menu").classList.toggle("change");
+    document.getElementById("navbar").classList.toggle("changem");
+    document.getElementById("backcolor").classList.toggle("changebg");
+}
